39ce2278ce9079ed5c975534740a8b1cc17d6031
yukta <iit2019052@iiita.ac.in>
2020-10-07 16:52:49 +0530

    Created Mainpage, UserLogin, AdminLogin UI

---
+++ AirlineBookingSystem/src/airlinebookingsystem/AirlineBookingSystem.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airlinebookingsystem;
+
+/**
+ *
+ * @author yukta
+ */
+public class AirlineBookingSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainScreen mr=new MainScreen();
+        mr.setVisible(true);
+        
+        // TODO code application logic here
+    }
+    
+}
