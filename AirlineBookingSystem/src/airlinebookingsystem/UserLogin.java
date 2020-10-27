e5a23d51f3e6c285437c65106ad7eb68ed3250d6
yukta <iit2019052@iiita.ac.in>
2020-10-27 22:23:45 +0530

    Setup SQL Database and configured the functionality to register

--- AirlineBookingSystem/src/airlinebookingsystem/UserLogin.java
+++ AirlineBookingSystem/src/airlinebookingsystem/UserLogin.java
@@ -63,6 +63,11 @@ public class UserLogin extends javax.swing.JFrame {
 
         jButton3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jButton3.setText("Create Account");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jPasswordField1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jPasswordField1.addActionListener(new java.awt.event.ActionListener() {
@@ -136,6 +141,14 @@ public class UserLogin extends javax.swing.JFrame {
         // TODO add your handling code here:
       
     }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        dispose();
+        NewUser nu=new NewUser();
+        nu.setLocationRelativeTo(null);
+        nu.setVisible(true);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
